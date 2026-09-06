ce9c64da3de435a7793106ba1fbf44475f66b81b
Maëlick Claes <himself@maelick.net>
2026-09-06 16:33:39 +0000

    feat: make fashion and tag domain types (de)serializable

--- core/src/domain/fashion.rs
+++ core/src/domain/fashion.rs
@@ -2,8 +2,9 @@ use bon::Builder;
 use chrono::{DateTime, Utc};
 
 use gw2fashionista_chatlink::templates::{travel::TravelTemplate, wardrobe::WardrobeTemplate};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Eq, PartialEq, Builder)]
+#[derive(Debug, Clone, Eq, PartialEq, Builder, Deserialize, Serialize)]
 pub struct Fashion {
     #[builder(into)]
     pub id: Option<uuid::Uuid>,
@@ -17,8 +18,10 @@ pub struct Fashion {
     #[builder(into)]
     pub character: Option<String>,
 
+    #[serde(with = "display_fromstr_option")]
     pub wardrobe_template: Option<WardrobeTemplate>,
 
+    #[serde(with = "display_fromstr_option")]
     pub travel_template: Option<TravelTemplate>,
 
     pub created_at: Option<DateTime<Utc>>,
@@ -27,4 +30,28 @@ pub struct Fashion {
 
     #[builder(default, into)]
     pub tags: Vec<String>,
+}
+
+mod display_fromstr_option {
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use std::{fmt::Display, str::FromStr};
+
+    pub fn serialize<T, S>(value: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: Display,
+        S: Serializer,
+    {
+        value.as_ref().map(T::to_string).serialize(serializer)
+    }
+
+    pub fn deserialize<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+    where
+        T: FromStr,
+        T::Err: Display,
+        D: Deserializer<'de>,
+    {
+        Option::<String>::deserialize(deserializer)?
+            .map(|s| s.parse().map_err(serde::de::Error::custom))
+            .transpose()
+    }
 }
